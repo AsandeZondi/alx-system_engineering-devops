59e5b08bfcf6aff56dd5345bee33ea20a4f05786
AsandeZondi <zondiasande10@gmail.com>
2024-03-27 17:19:42 -0700

    createFile

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Creates a file using puppet
+file {'/tmp/school':
+path => '/tmp/school',
+ensure => 'file',
+mode => '0744',
+owner => 'www-data',
+group => 'www-data',
+content => 'I love Puppet',
+}
